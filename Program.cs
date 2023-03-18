dc6ac55fbf982f08ae1e126cf39558df28b7cdd5
dleuiajs <jduejdb322@gmail.com>
2023-03-18 23:50:03 +0200

    added fast open links

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace SteamGamesRemover
 {
@@ -9,26 +10,22 @@ namespace SteamGamesRemover
             bool end = false;
             //int[] idGames = new int[];
             List<int> idGames = new List<int> { };
-            string links = "";
-            int i = 0;
+            List<string> links = new List<string> { };
             Console.WriteLine("Введите айди игр (после ввода 1 айди нажимайте Enter и снова вводите). При окончании ввода напишите /s");
             while (!end) // читание нескольких строк
             {
-                string textRead = Console.ReadLine();
+                string? textRead = Console.ReadLine();
                 if (textRead != "/s")
                 {
                     bool success = int.TryParse(textRead, out int num);
                     if (success)
                     {
-                        idGames.Add(idGames.Count + 1);
-                        idGames[i] = num;
-                        i++;
+                        idGames.Add(num);
                     }
                     else
                     {
                         Console.WriteLine("Неверный айди! Указывайте только число, например, 1000");
                     }
-
                 }
                 else
                 {
@@ -38,9 +35,20 @@ namespace SteamGamesRemover
             Console.WriteLine("Получение ссылок...");
             for (int n = 0; n < idGames.Count; n++)
             {
-                links += "\nhttps://help.steampowered.com/ru/wizard/HelpWithGameIssue/?appid=" + idGames[n] + "&issueid=123";
+                links.Add("https://help.steampowered.com/ru/wizard/HelpWithGameIssue/?appid=" + idGames[n] + "&issueid=123");
+                Console.WriteLine(links[n]);
             }
-            Console.WriteLine(links);
+            Console.WriteLine("Хотите открыть ссылки? (Y - Yes, N - No)");
+            string? confirmation = Console.ReadLine();
+            {
+                if (confirmation.ToLower() == "y" || confirmation.ToLower() == "yes")
+                {
+                    for (int n = 0; n < links.Count; n++)
+                    {
+                        Process.Start(new ProcessStartInfo("cmd", $"/c start {links[n]}") { CreateNoWindow = true });
+                    }
+                }
+            }
             Console.WriteLine("Нажмите любую клавишу для выхода");
             Console.ReadKey(true);
         }
